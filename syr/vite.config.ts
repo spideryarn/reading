0fdee8f13aeeb6add0258774e7799ed716c49800
Greg Detre <greg@gregdetre.co.uk>
2025-02-02 03:06:11 +0000

    Updated tests

--- syr/vite.config.ts
+++ syr/vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	test: {
+		include: ['tests/unit/**/*.{test,spec}.{js,ts}'],
+		exclude: ['tests/frontend/**/*', 'tests-examples/**/*']
+	}
 });
